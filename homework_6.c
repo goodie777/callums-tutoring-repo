3ce90a6bf5d5d99ab30890f823dcf56ea5ffa811
11cconnolly <callumc11@gmail.com>
2026-07-06 13:40:22 +0100

    homework_6 commit, make a pull request to change

---
+++ homework_6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void greet(char name[]);
+int add(int a, int b);
+void print_result(int result);
+
+/* 
+ * Questions to answer here, or underneath in the code as additional comments:
+ * 
+ * - Which variables are local?
+ * 
+ * - Which variables are parameters?
+ * 
+ * - Which variables are visible in each function?
+ * 
+ */
+
+int main(void) {
+    // Start of program, call greet() to greet the user
+
+    // Call add() to add two numbers, store the result in a variable
+
+    // Call print_result() to print the result of the addition
+
+    // Return 0 to indicate successful completion of the program
+}
+
+void greet(char name[]) {
+    
+}
+
+int add(int a, int b) {
+
+}
+
+void print_result(int result) {
+
+}
